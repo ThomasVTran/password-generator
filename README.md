ed5c024edaace627b8c24550929cae687f11568e
Thomas Tran <2001thomas.tran@gmail.com>
2023-12-22 17:41:25 -0600

    added readme.md, arrays, math.floors, finished readme.md

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# password-generator
-First password generator
+# Title
+Password-generator
+
+## Description
+This password generator represents my first introduction toward JavaScript by allowing me to come up with my own solutions for some rudimentary starting code. Although this doesn't yet get into the back end of development, it does give me a peak into what that world will soon look like. Now users can visit this mini project and generate a password as long as they provide a few small parameters. Given they have provided a character amount between a minumum of 8 and a maximum of 128, character options for the random password will vary.  
+
+## Usage
+![Img](./127.0.0.1_5500_index.html.png)
+
+## Git hub
+ [Deployed link](https://thomasvtran.github.io/password-generator/)
--- script.js
+++ script.js
@@ -3,49 +3,66 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
   // ALL logic and vars here
-   console.log("You clicked the button!")
-  
+  console.log("You clicked the button!")
+  var finalPassword =[]
+  var randomPassword = []
+  //  b.Lowercase, uppercase, numbers, special characters
+  var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+  var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+  var number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,]
+  var character = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"]
   //1.Prompt users
+  //  a.Password length 8 < 128
   var passwordLength = prompt("Choose password length (must be between 8 and 128)")
   console.log(passwordLength);
-  if (passwordLength > 7 && passwordLength < 129 ) {
-    var upper = alert("Would you like uppercases?")
-    if (upper === true) {
-      var upper = Math.floor(Math.random() * upper.length);
-    } else {
-      
+  if (passwordLength > 7 && passwordLength < 129) {
+    var upperPassword = confirm("Would you like uppercases?")
+    if (upperPassword === true) {
+      var upperRandom = Math.floor(Math.random() * upper.length);
+      console.log(upper[upperRandom])
+      finalPassword.push(upper[upperRandom])
+      randomPassword= randomPassword.concat(upper)
     }
+    var lowerPassword = confirm("Would you like lowercases?")
+    if (lowerPassword === true) {
+      var lowerRandom = Math.floor(Math.random() * lower.length);
+      console.log(lower[lowerRandom])
+      finalPassword.push(lower[lowerRandom])
+      randomPassword= randomPassword.concat(lower)
+    }
+    var numberPassword = confirm("Would you like numbers?")
+    if (numberPassword === true) {
+      var numberRandom = Math.floor(Math.random() * number.length);
+      console.log(number[numberRandom])
+      finalPassword.push(number[numberRandom])
+      randomPassword= randomPassword.concat(number)
+    }
+    var characterPassword = confirm("Would you like special characters?")
+    if (characterPassword === true) {
+      var characterRandom = Math.floor(Math.random() * character.length);
+      console.log(character[characterRandom])
+      finalPassword.push(character[characterRandom])
+      randomPassword= randomPassword.concat(character)
+    }
+    if (upperPassword === false && lowerPassword === false && numberPassword === false && characterPassword === false)
+      alert("Must have at least 1 uppercase letter, lowercase letter, number, or special characcer")
+
   } else {
     alert("Uh oh! You either chose too many characters or not enough. Try again!")
   }
-  
 
-  //  a.Password length 8 < 128
-  //  b.Lowercase, uppercase, numbers, special characters
-  var upper= ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-  var lower=["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-  // var lower = alert("Would you like lowercases?")
-  //   if (lower === true) {
-  //     var lower = Math.floor(Math.random() * lower.length);
-  //   } else {
-  var number=[0,1,2,3,4,5,6,7,8,9,]
-  // var number = alert("Would you like numbers??")
-  //   if (number === true) {
-  //     var number = Math.floor(Math.random() * number.length);
-  //   } else {
-  var character=["!","@","#","$","%","^","&","*","(",")"]
-  // var character = alert("Would you like special characters?")
-  //   if (character === true) {
-  //     var character = Math.floor(Math.random() * character.length);
-  //   } else {
+  for (var i = finalPassword.length; i < passwordLength; i++) {
+    finalPassword.push(randomPassword[Math.floor(Math.random() * randomPassword.length)])
+  }
+  
   //2.Validate the input
   //3.Generate password based on criteria
-  
-  
+
+
   //4.Display generated password to page
-    return "Generated password goes here!"
-  }
-  // var 
+  return finalPassword.join("")
+}
+// var 
 
 
 // Write password to the #password input
